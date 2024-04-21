a9456e897687faf51f9f66d5dd545853c84d4284
Jagruti patil <jagrutikpatil@98gmail.com>
2024-04-21 18:06:05 +0530

    Collection first commit

---
+++ src/main/java/com/collection/collection_practice/ListDemo.java
@@ -0,0 +1,13 @@
+package com.collection.collection_practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDemo {
+	public static void main(String[] args) {
+		List<Integer> list =Arrays.asList(2,4,5,6,7,1,2,4,6);
+		list.forEach(System.out::println);
+
+	}
+	
+}
